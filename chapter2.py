be3f1f4e160c6b72fee9e83dfbb14bd5fa0920a2
Ross Russell <ross.russell.ca@gmail.com>
2018-01-20 20:55:59 -0800

    word functions

--- chapter2.py
+++ chapter2.py
@@ -51,21 +51,38 @@ def diamond():
    number of words, identifying the length of the longest word, 
    the greatest number of vowels in a word, and/or any other 
    statistics you can think of.'''
+def get_word_lengths(words:list)->dict:
+    '''
+    Clean array in, clean dict out. Otherwise
+    punctuation will be counted.
+    :param words: array w/o punctuation.
+    :return: dict of word keys and number of chars values
+    '''
+    sorted = {}
+    for i in words:
+        sorted[i] = len(i)
+    return sorted
 
 def wordStatistics(textLine:str):
     cleanUp = textLine.split(' ')
     wordList = []
     tmp = ''
-    for item in cleanUp: #Sort out all garbage
-                         #Actually, this seems like a great time
-                         #to sort into a dictionary for more statistics.
+    #Clean out special chars
+    for item in cleanUp:
         for letter in item:
             if letter.isalpha():
                 tmp += letter
         wordList.append(tmp)
         tmp = ''
 
+    #Get the longest and shortest words.
+    countedWords = get_word_lengths(wordList)
+    longestWord = max(countedWords)
+    shortestWord = min(countedWords)
+    biggest = countedWords[max(countedWords)]
+    smallest = countedWords[min(countedWords)]
 
     print('Word count:', len(wordList))
-    print('Longest word:')
+    print('Longest word:', longestWord, 'at', biggest, 'characters.' )
+    print('Shortest word:', shortestWord, 'at', smallest, 'characters.')
 wordStatistics('hello, this is a wordlist.')
